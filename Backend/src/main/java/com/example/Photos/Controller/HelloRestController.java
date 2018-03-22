964149d505338ff885373137bfe2fa7540f80718
Adam <adam.clark2@maine.edu>
2018-03-22 10:56:50 -0400

    Added jdbc

--- Backend/src/main/java/com/example/Photos/Controller/HelloRestController.java
+++ Backend/src/main/java/com/example/Photos/Controller/HelloRestController.java
@@ -3,6 +3,13 @@ package com.example.Photos.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 @RestController
 public class HelloRestController {
 
@@ -21,5 +28,52 @@ public class HelloRestController {
     public class message{
         public String hello = "world";
         public String world = "helllo";
+    }
+
+    @RequestMapping(value = "/dbTest", produces = "text/plain")
+    public String database(){
+        return doDatabase();
+    }
+
+    public static String doDatabase(){
+        try {
+            Connection c = DriverManager.getConnection("jdbc:mysql://" + System.getenv("DATABASE_URL"));
+            PreparedStatement p = c.prepareStatement("SELECT * FROM helloworld");
+            ResultSet rs = p.executeQuery();
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            PrintWriter pr = new PrintWriter(bos);
+
+            // Header
+            int colCount = rs.getMetaData().getColumnCount();
+            for(int i = 1; i < colCount;i++){
+                pr.printf("%20s|", rs.getMetaData().getColumnName(i) + "/" + rs.getMetaData().getColumnTypeName(i) );
+            }
+            pr.printf("%20s", rs.getMetaData().getColumnName(colCount) + "/" + rs.getMetaData().getColumnTypeName(colCount - 1));
+            pr.println();
+            for(int i = 0;i < (20 + 1) * colCount;i++){
+                pr.printf("-");
+            }
+            pr.println();
+
+            // Data
+            while(rs.next()){
+                for(int i = 1; i < colCount;i++){
+                    pr.printf("%20s|", rs.getString(i));
+                }
+                pr.printf("%20s", rs.getString(colCount));
+                pr.println();
+            }
+            c.close();
+            pr.flush();
+
+            return new String(bos.toByteArray());
+
+
+        }catch (Exception e){
+            // OOPS
+            e.printStackTrace();
+            return "xyz" + e.getLocalizedMessage() + e.getMessage();
+        }
     }
 }
--- Backend/src/main/java/com/example/Photos/PhotosApplication.java
+++ Backend/src/main/java/com/example/Photos/PhotosApplication.java
@@ -1,12 +1,23 @@
 package com.example.Photos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.Environment;
+
+import javax.inject.Inject;
+import javax.swing.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PhotosApplication extends SpringBootServletInitializer {
+
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(PhotosApplication.class);
